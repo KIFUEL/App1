cf88c3b618c03c42b5d5b3ad6c4fd9cbdbed8ce3
Ulises- <ulises_isais@hotmail.com>
2021-10-27 22:15:53 -0500

    Base de datos

---
+++ dbo.Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE tutor_bees(
+Calificar int,
+Materia varchar (64) constraint mat_tu_nn not null
+);
+-- alter table tutor_bees add constraint usuario_tu_fk foreign key(ID_Usuario) references usuarios_bees(ID_Usuario);
